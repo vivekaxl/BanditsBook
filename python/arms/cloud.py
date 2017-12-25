fbb403a6f15df3b111d0ad5105aab4e186b4f201
Vivek <vivekaxl@gmail.com>
2017-12-24 23:57:42 -0500

    first attempt

---
+++ python/arms/cloud.py
@@ -0,0 +1,20 @@
+import random
+
+
+class CloudArm():
+    def __init__(self, data):
+        self.data = [1/d for d in data]
+        # used to keep track
+        self.counts = [0 for _ in xrange(len(self.data))]
+
+    def draw(self):
+        # find workload which has not been measured
+        min_value = min(self.counts)
+        # assert(min_value==0), "Every workload is evaluated"
+        not_selected = [i for i,c in enumerate(self.counts) if c == min_value]
+        random.shuffle(not_selected)
+        min_workload_id = not_selected[0]
+        self.counts[min_workload_id] += 1
+        return self.data[min_workload_id]
+
+
--- python/demo.py
+++ python/demo.py
@@ -1,48 +1,81 @@
-execfile("core.py")
+from __future__ import division
+# Need access to random numbers
+import random
 
-arm1 = BernoulliArm(0.7)
-arm1.draw()
-arm1.draw()
+# Definitions of bandit arms
+from arms.adversarial import *
+from arms.bernoulli import *
+from arms.normal import *
+from arms.cloud import *
 
-arm2 = NormalArm(10.0, 1.0)
-arm2.draw()
-arm2.draw()
+# Definitions of bandit algorithms
+from algorithms.epsilon_greedy.standard import *
+from algorithms.epsilon_greedy.annealing import *
+from algorithms.softmax.standard import *
+from algorithms.softmax.annealing import *
+from algorithms.ucb.ucb1 import *
+from algorithms.ucb.ucb2 import *
+from algorithms.exp3.exp3 import *
+from algorithms.hedge.hedge import *
 
-arm3 = BernoulliArm(0.2)
-arm3.draw()
-arm3.draw()
+# # Testing framework
+from testing_framework.tests import *
+import os
+import pandas as pd
 
-arms = [arm1, arm2, arm3]
 
-n_arms = len(arms)
+# Convenience functions
+def cloud_arms(data_file):
+    content = pd.read_csv(data_file)
+    vm_types = sorted([c for c in content.columns if '.' in c])  # to make sure we get the same instance order
+    arms = []
+    for vm_type in vm_types:
+        arms.append(CloudArm(content[vm_type].values.tolist()))
+    return arms, vm_types
 
-algo1 = EpsilonGreedy(0.1, [], [])
-algo2 = Softmax(1.0, [], [])
-algo3 = UCB1([], [])
-algo4 = Exp3(0.2, [])
+data_folder = "./data/"
+datas = [data_folder + d for d in os.listdir(data_folder) if '.csv' in d]
 
-algos = [algo1, algo2, algo3, algo4]
+for data in datas:
+    arms, vm_types = cloud_arms(data)
+    n_arms = len(arms)
+    algo1 = EpsilonGreedy( 0.1, [], [])
+    algo2 = Softmax( 1.0, [], [])
+    algo3 = UCB1( [], [])
+    # algo4 = Exp3( 0.2, [])
 
-for algo in algos:
-  algo.initialize(n_arms)
+    algos = [algo1, algo2, algo3]
 
-for t in range(1000):
-  for algo in algos:
-    chosen_arm = algo.select_arm()
-    reward = arms[chosen_arm].draw()
-    algo.update(chosen_arm, reward)
+    for algo in algos:
+      algo.initialize(n_arms)
 
-algo1.counts
-algo1.values
+    for t in range(40):
+      for algo in algos:
+        chosen_arm = algo.select_arm()
+        reward = arms[chosen_arm].draw()
+        algo.update(chosen_arm, reward)
 
-algo2.counts
-algo2.values
+    print data
+    print "Epsilon Greedy"
+    for v, cn in zip(vm_types, algo1.counts):
+        print v[:5], cn, "| ",
+    print
 
-algo3.counts
-algo3.values
+    print "Softmax"
+    for v, cn in zip(vm_types, algo2.counts):
+        print v[:5], cn, "| ",
+    print
 
-algo4.weights
+    print "UCB1"
+    for v, cn in zip(vm_types, algo3.counts):
+        print v[:5], cn, "| ",
+    print
 
+    raw_input()
+
+
+"""
 num_sims = 1000
 horizon = 10
 results = test_algorithm(algo1, arms, num_sims, horizon)
+"""
